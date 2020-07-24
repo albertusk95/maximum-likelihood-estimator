c8a68547afef2e0b956fd79e58befd8814217d86
Albertus Kelvin <albertus.kelvin@gdplabs.id>
2020-07-24 08:23:12 +0700

    init binomial

--- src/main/scala/stats/configs/FittedDistrConfig.scala
+++ src/main/scala/stats/configs/FittedDistrConfig.scala
@@ -5,4 +5,5 @@ case class FittedNormalDistrConfig(column: String, source: SourceConfig)
 
 case class FittedExpDistrConfig(column: String, source: SourceConfig) extends BaseFittedDistrConfig
 
-case class FittedBinomialDistrConfig(column: String, source: SourceConfig, successEvent: String) extends BaseFittedDistrConfig
+case class FittedBinomialDistrConfig(column: String, source: SourceConfig, successEvent: String)
+    extends BaseFittedDistrConfig
